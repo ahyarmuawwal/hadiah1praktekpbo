f58d218d332a4e63ab11993dc0a261f00a3a4129
ahyarmuawwal <ahyarmuawwal.xi.ia2@gmail.com>
2020-03-12 15:06:39 +0800

    Add files via upload

---
+++ hello.java
@@ -0,0 +1,9 @@
+import java.io.*;
+class Hello{
+	public static void main (String[] arg)
+	{
+		System.out.println("hallo ini belajar java");
+		System.out.println("nama saya ahyar");
+		System.out.println("ini kelas java");
+	}
+}
